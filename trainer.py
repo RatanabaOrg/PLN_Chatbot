617884173f36ff0f0864403351a0f66581cb9d09
JuliaGayotto <juliagayotto@gmail.com>
2024-11-24 20:36:58 -0300

    adicao da correcao de caracteres e da remocao de caracteres repetidos

--- trainer.py
+++ trainer.py
@@ -5,7 +5,6 @@ trainningList = [
             "oi",
             "ajuda",
             "ajudar",
-            "qual é o seu nome?",
             "qual seu nome?",
         ],
         "Olá, eu sou a Bety, sua assistente virtual! Como posso ajudar você hoje?"
@@ -16,19 +15,112 @@ trainningList = [
             "acessos recentes",
             "últimos acessos",
             "último acesso",
-            "acesso mais recente",
-            "acesso recente"
+            "filtrar acesso",
+            "histórico acessos",
+            "acessos hoje",
         ],
         
-        "Você pode encontrar na página de últimos acessos, localizada no topo do menu lateral à esquerda."
+        "Você pode encontrar os acessos na página histórico, localizada no menu lateral à esquerda. Ainda nessa página, é possível filtrar os acessos mais recentes utilizando o filtro no canto superior direito."
+    ],
+    [   
+        [
+            "visualizar alertas",
+            "historico alertas",
+            "visualizar alerta",
+            "historico alerta",
+            "filtrar alerta"
+        ],
+        
+        "Você pode encontrar o histórico de alertas na página alertas, localizada no menu lateral à esquerda. Ainda nessa página, é possível filtrar os alertas por tipos: moderado, alto ou crítico no canto superior direito."
+    ],
+    [   
+        [
+            "gera alerta",
+            "alerta gerado",
+            "gerar alerta",
+        ],
+        
+        "Um alerta é gerado toda vez que uma pessoa não autorizada passa pela porta de uma área restrita. Esse alerta pode ser moderado, alto ou crítico."
+    ],
+    [   
+        [
+            "acesso porta",
+            "acesso área",
+        ],
+        
+        "Cada vez que uma pessoa acessa a porta de uma área restrita é gerado uma notificação para que seja possível visualizar quem acessou e para determinar se aquele acesso foi autorizado ou não. Essa notificação é exibida em todas as telas da aplicação."
+    ],
+    [   
+        [
+            "dashbord",
+            "acessos dia",
+            "acessos tempo",
+            "tempo sem acesso"
+        ],
+        
+        "Você pode encontrar a tela de dashboard no menu lateral à esquerda. Nela é possível visualizar o número de acessos e o tempo de sem acesso do sistema e de uma área especifica. Esse filtro por área especifíca pode ser feito selecionando a área desejada no filtro localizado no canto superior direito."
     ],
     [
         [
+            "deslogar",
+            "sair",
+            "sair conta",
+        ],
+
+        "Para deslogar, clique no botao localizado abaixo do menu lateral à esquerda."
+    ],
+    [
+        [
+            "criar conta",
+            "criar usuário",
+            "cadastrar usuário",
+        ],
+
+        "Para que um novo usuário seja criado deve ser feito o cadastro deste na página cadastrar disponível na tela de login. Para que esse usuário consiga acessar o sistema essa conta deverá ser aprovada por um administrador primeiro."
+    ],
+    [
+        [
+            "aprovar conta",
+            "aprovar usuário",
+            "aprovar usuários",
+            "aceitar conta",
+            "aceitar usuário",
+            "aceitar usuários",
+        ],
+
+        "Para aprovar um usuário vá até a pagina visualizar, na seção USUÁRIOS, localizada no menu lateral à esquerda. Nessa página irá aparecer todos os usuários que solicitaram acesso ao sistema. Para aprovar uma conta específica, localize o usuário que vê deseja aprovar e no botão verde de aprovação e selecione o cargo daquela conta (funcionário ou administrador)."
+    ],
+    [
+        [
+            "editar usuário",
+            "editar usuários",
+            "editar dados usuário",
+            "editar nome usuário",
+            "editar email usuário",
+            "editar telefone usuário"
+        ],
+
+        "Para editar as informações de um usuário, entre na página visualizar, na seção USUÁRIOS, localizada no menu lateral à esquerda, localize o usuário que você deseja alterar as informações e clique no botão azul com formato de ficha. Após alterar as informações, não se esqueça de clicar no botão de salvar!"
+    ],
+    [
+        [
+            "excluir usuário",
+            "excluir usuários",
+            "deletar usuário",
+            "deletar usuários",
+        ],
+
+        "Para deletar um usuário, acesse a página visualizar, na seção USUÁRIOS, localizada no menu lateral à esquerda. Localize o usuário que deseja deletar e clique no botão vermelho em formato de lixo para deletar."
+    ],
+    [
+        [
+            "usuários cadastrados",
+            "visualizar usuário",
             "visualizar usuários",
             "visualizar todos usuários",
         ],
 
-        "Clique na página de visualizar usuário no final do menu lateral."
+        "Clique na página de visualizar, na seção USUÁRIOS, localizada no final do menu lateral à esquerda."
     ],
     [
         [
@@ -38,6 +130,54 @@ trainningList = [
             "visualizar todas áreas",
         ],
 
-        "Você pode visualizar as áreas cadastradas na página Visualizar, localizada na seção ACESSOS, no menu lateral."
-    ]
+        "Você pode visualizar as áreas cadastradas na página Visualizar, localizada na seção ÁREAS, no menu lateral à esquerda."
+    ],
+    [
+        [
+            "cadastrar áreas",
+            "cadastrar área",
+            "cadastro área",
+            "criar área",
+            "crio área",
+        ],
+
+        "Você pode criar uma nova área restrita na página cadastrar, na seção ÁREAS, localizada no menu lateral à esquerda."
+    ],
+    [
+        [
+            "editar área",
+            "editar áreas",
+            "editar dados área",
+            "editar nome área",
+            "editar código área",
+            "editar nível de segurança área",
+            "editar localização área",
+            "editar descrição área",
+        ],
+
+        "Para editar as informações de uma área, entre na página visualizar, na seção ÁREAS, localizada no menu lateral à esquerda, localize a área que você deseja alterar as informações e clique no botão azul com formato de ficha. Após alterar as informações, não se esqueça de clicar no botão de salvar!"
+    ],
+    [
+        [
+            "excluir área",
+            "excluir áreas",
+            "deletar área",
+            "deletar áreas",
+        ],
+
+        "Para deletar uma área, acesse a página visualizar, na seção ÁREAS, localizada no menu lateral à esquerda. Localize a área que deseja deletar e clique no botão vermelho em formato de lixo para deletar."
+    ],
+    [   
+        [
+            "aprovar acesso",
+            "aprovo acesso",
+            "vizualizar acesso porta",
+            "vizualizar acessos porta",
+            "vizualizar acesso área",
+            "vizualizar acessos área",
+        ],
+        
+        "Toda vez que uma porta de uma área restrita é aberta surge uma notificação na aplicação. Clicando na notificação, é posível dizer se aquele acesso foi autorizado ou não e emitir um alerta caso não tenha sido autorizado."
+    ],
+
 ]
